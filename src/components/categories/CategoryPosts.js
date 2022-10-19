8b943a3ed973bc9eb00eca6198fb334c593f606c
carlk <carloskachuk@gmail.com>
2022-10-19 09:54:23 -0300

    the bug of not displaying the posts was fixed

--- src/components/categories/CategoryPosts.js
+++ src/components/categories/CategoryPosts.js
@@ -10,18 +10,16 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 const CategoryPosts = ()=>{
     const {categoryId}= useParams()
-
     const {loading, error, data: dataPosts} = useQuery('posts', async()=>{
         const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/category/${categoryId}`)
         return response.data
     })
 
-
     return(
         <Box className="divCategory" sx={{ display: 'flex', justifyContent: 'center', flexDirection:'column'}}>
             {error ? (<Error error={error}/>) : (null)}
             {loading? <CircularProgress/> : (null)}
-            {dataPosts.posts.length > 0 ? <Posts posts={dataPosts.posts}/> :
+            {dataPosts.posts.length !== 0 ? <Posts posts={dataPosts.posts}/> :
              (<>
                 <Typography variant="h5"  sx={{ display:'flex', justifyContent:'center', mr:100, mb:4, mt:6 }}>Projects</Typography>
                 <IsEmpty/>
